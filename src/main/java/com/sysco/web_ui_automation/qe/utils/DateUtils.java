bf8aa2da53bff547a277ac608c417162d616b56b
gher1098 <Herath.Gayan@corp.sysco.com>
2018-08-12 22:21:42 +0530

    Fixed Sonar Violations in DateUtils

--- src/main/java/com/sysco/web_ui_automation/qe/utils/DateUtils.java
+++ src/main/java/com/sysco/web_ui_automation/qe/utils/DateUtils.java
@@ -1,30 +1,11 @@
 package com.sysco.web_ui_automation.qe.utils;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
-
-    public static String getCurrentDate() {
-        Date date = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy");
-        String currentDate = sdf.format(date);
-        return currentDate;
-    }
-
-    public static String getFutureDate(int numOfDays) {
-        Date date = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.DATE, numOfDays);
-        date = calendar.getTime();
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
-        String futureDate = sdf.format(date);
-
-        return futureDate;
-    }
+    private DateUtils(){}
 
     public static String getPastOrFutureDate(int numOfYears,int numOfDays) {
         Date date = new Date();
@@ -39,16 +20,4 @@ public class DateUtils {
         return pastDate;
     }
 
-    public static String getDayOfWeekOfGivenDate(String date) {
-        String dayOfWeek = "";
-        try {
-            SimpleDateFormat format1 = new SimpleDateFormat("MM/dd/yyyy");
-            Date dt1 = format1.parse(date);
-            DateFormat format2 = new SimpleDateFormat("EEEE");
-            dayOfWeek = format2.format(dt1);
-        } catch (Exception e) {
-
-        }
-        return dayOfWeek;
-    }
 }
